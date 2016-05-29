c849b532693eeb3d50f78d56f78ea446d6dffc85
matiboy <mathieu.chauvinc@gmail.com>
2016-05-29 15:46:25 +0800

    Can t use .d like that

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-/// <reference path="./node_modules/typescript/lib/lib.core.es6.d.ts" />
-/// <reference path="./node_modules/@angular/core/testing.d.ts"/>
 export {PrettyJsonPipe} from "./src/prettyjson.pipe";
 export {SafeJsonPipe} from "./src/json.pipe";
 export {PrettyJsonComponent} from "./src/prettyjson.component";
