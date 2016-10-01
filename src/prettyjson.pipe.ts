041d714c25fbaa93d8d3e986d52b7fad452541da
Vv <vv@targate.fr>
2016-10-01 08:15:31 +0200

    fix : only test undefined + return span with undefined class

--- src/prettyjson.pipe.ts
+++ src/prettyjson.pipe.ts
@@ -11,10 +11,10 @@ export class PrettyJsonPipe implements PipeTransform {
   }
 
   private _syntaxHighlight(json: any, serializer: any, spacing: number): string {
-    // Credits to the accepted answer here http://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
-    if (!json) {
-        return "";
+    if (json === undefined) {
+        return '<span class="undefined"></span>';
     }
+    // Credits to the accepted answer here http://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
     if (typeof json !== "string") {
       json = JSON.stringify(json, serializer, spacing);
     }
