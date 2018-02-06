c8bf35cad2f7f1fdd4c7a3d8163bbae7adbfd776
matiboy <mathieu.chauvinc@gmail.com>
2018-02-07 06:02:40 +0800

    Some linting stuff

--- src/prettyjson.pipe.ts
+++ src/prettyjson.pipe.ts
@@ -1,25 +1,26 @@
 import {Pipe, PipeTransform} from "@angular/core";
-import {serializer} from "./safe.util";
+import {serializer as circularSerializer} from "./safe.util";
 
 @Pipe({
   name: "prettyjson",
   pure: false
 })
 export class PrettyJsonPipe implements PipeTransform {
-  transform(obj: any, spaces = 2): string {
-    return this._syntaxHighlight(obj, serializer(), spaces);
+  public transform(obj: any, spaces = 2): string {
+    return this._syntaxHighlight(obj, circularSerializer(), spaces);
   }
 
   private _syntaxHighlight(json: any, serializer: any, spacing: number): string {
     if (json === undefined) {
         return '<span class="undefined"></span>';
     }
-    // Credits to the accepted answer here http://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
+    // Credits to the accepted answer here
+    // http://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
     if (typeof json !== "string") {
       json = JSON.stringify(json, serializer, spacing);
     }
     json = json.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match: any) {
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, (match: any) => {
         let cls = "number";
         if (/^"/.test(match)) {
             if (/:$/.test(match)) {
