0c5b4347087a30172e75c9d2b79d757fbd1e5b6d
matiboy <mathieu.chauvinc@gmail.com>
2018-02-07 06:50:46 +0800

    Lint related stuff

--- src/safe.util.ts
+++ src/safe.util.ts
@@ -1,25 +1,24 @@
 // Taken from github.com/isaacs/json-stringify-safe
 export function serializer() {
-  let stack: any[] = [];
-  let keys: string[] = [];
+  const stack: any[] = [];
+  const keys: string[] = [];
 
-  let cycleReplacer = function(key: string, value: any) {
+  const cycleReplacer = function(key: string, value: any) {
     if (stack[0] === value) {
       return "[Circular ~]";
     }
     return "[Circular ~." + keys.slice(0, stack.indexOf(value)).join(".") + "]";
   };
 
-  return function(key: string, value: any) {
+  return function(this: any, key: string, value: any) {
     if (stack.length > 0) {
-      let thisPos = stack.indexOf(this);
+      const thisPos = stack.indexOf(this);
       ~thisPos ? stack.splice(thisPos + 1) : stack.push(this);
       ~thisPos ? keys.splice(thisPos, Infinity, key) : keys.push(key);
       if (~stack.indexOf(value)) {
         value = cycleReplacer.call(this, key, value);
       }
-    }
-    else {
+    } else {
       stack.push(value);
     }
 
